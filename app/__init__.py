75771630ef6e9c68612e2294fbcf470ebfa47f7c
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-14 18:24:02 +0300

    create send email functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,6 +19,7 @@ def create_app(config_name):
     # Initializing Flask Extensions
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # configure UploadSet
     configure_uploads(app,photos)
--- config.py
+++ config.py
@@ -6,3 +6,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alufwany:alufwany@localhost/pitch
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
