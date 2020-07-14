a97509df846c836f605a1f7969c0e13ed80babc1
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-14 16:47:23 +0300

    create a registration form

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,16 @@
-from flask import render_template, redirect,url_for,flash,request
-from . import auth
-from .forms import RegistrationForm,LoginForm
-from .. import db
-from ..models import User
-from flask_login import login_user, logout_user, login_required,current_user
-from ..email import mail_message
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import Pitch_Form, Update_Profile,CommentsForm
+from .. import db, photos
+from flask_login import login_required, current_user
 
-@auth.route('/login',methods=['GET','POST'])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+#Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns index page and its data
+    '''
 
-        flash('Invalid username or Password')
+    title = 'Home - Welcome to The Pitches of The Century'
+    return render_template('index.html', title = title)
