1e6346ed2e27aaa801a91666dd7f063109ddf1bd
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-14 16:20:20 +0300

    add auth blueprint class

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    db.init_app(app)
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alufwany:alufwany@localhost/pitch
