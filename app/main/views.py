c7e8672d22863bd4200a41e11f0c6f84c4fe5029
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-14 17:36:17 +0300

    create a template file for the profile

--- app/main/views.py
+++ app/main/views.py
@@ -14,3 +14,12 @@ def index():
 
     title = 'Home - Welcome to The Pitches of The Century'
     return render_template('index.html', title = title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
